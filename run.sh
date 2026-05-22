08bad0f32c898eb815997412b3e8c7a9412c6c82
Stephen Swensen <stephen.swensen@gmail.com>
2026-05-21 23:34:52 -0400

    solve problem 1

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-fcc /usr/local/share/fcc/stdlib/*.fc main.fc -o /tmp/euler-main.c
+STDLIB=/usr/local/share/fcc/stdlib
+fcc $STDLIB/io.fc main.fc -o /tmp/euler-main.c
 cc -std=c11 -Wall -Werror /tmp/euler-main.c -o /tmp/euler-main -lm
-/tmp/euler-main
+/tmp/euler-main "$1"
