06c7100ee2d6fe7de62f602bb1bec453fe06868d
Stephen Swensen <stephen.swensen@gmail.com>
2026-05-23 22:02:42 -0400

    timings

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 set -e
 STDLIB=/usr/local/share/fcc/stdlib
-fcc $STDLIB/io.fc main.fc -o /tmp/euler-main.c
+fcc $STDLIB/io.fc $STDLIB/sys.fc main.fc -o /tmp/euler-main.c
 cc -std=c11 -Wall -Werror -O2 /tmp/euler-main.c -o /tmp/euler-main -lm
 /tmp/euler-main "$1"
